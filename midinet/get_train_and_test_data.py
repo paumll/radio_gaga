7f3d2e162f9c438d74a73a1ceb3d7ae2b97af390
paumll <paumll1999@gmail.com>
2020-06-06 11:53:33 +0200

    Solved dimensions error

--- midinet/get_train_and_test_data.py
+++ midinet/get_train_and_test_data.py
@@ -4,10 +4,11 @@ import math
 import time
 
 #load data
-data = np.load('octave2_x_T.npy')
-prev_data = np.load('octave2_prev_x_T.npy')
+data = np.load('data_x.npy')
+prev_data = np.load('prev_x.npy')
+chord_data = np.load('chords.npy')
 print('data shape: {}'.format(data.shape))
-time.sleep(3)
+#time.sleep(3)
 
 song_idx = int(data.shape[0]/8)
 test_ratial = 0.1
@@ -15,7 +16,6 @@ test_song_num = round(song_idx*test_ratial)
 train_song_num = data.shape[0] - test_song_num
 print('total song number: {}'.format(song_idx))
 print('number of test song: {}, \n,number of train song: {}'.format(test_song_num,train_song_num))
-time.sleep(3)
 
 #create the song idx for test data
 
@@ -24,12 +24,10 @@ full = np.arange(song_idx)
 test_idx= random.sample(range(0,full.shape[0]),test_song_num)
 test_idx = np.asarray(test_idx)
 print('total {} song idx for test: {}'.format(test_idx.shape[0],test_idx))
-time.sleep(3)
 
 #create the song idx for train data
 train_idx = np.delete(full,test_idx)
 print('total {} song idx for train: {}'.format(train_idx.shape[0],train_idx))
-time.sleep(3)
 
     
 
@@ -40,8 +38,9 @@ def test_data(data,test_idx):
     for i in range(test_idx.shape[0]):
         stp = (test_idx[i])*8
         edp = stp + 8
-        song = data[stp:edp,0,:,:]
-        song = song.reshape((8,1,128,16))
+        song = data[stp:edp]
+        print(song.shape)
+        song = song.transpose(0, 1, 3, 2)
         X_te.append(song)
         # print('i: {}, test_iex: {}, stp: {}, song.shape: {}, song num: {}'.format(i, test_idx[i], stp, song.shape, len(X_te)))
         
@@ -56,8 +55,8 @@ def train_data(data,train_idx):
     for i in range(train_idx.shape[0]):
         stp = (train_idx[i])*8
         edp = stp + 8
-        song = data[stp:edp,0,:,:]
-        song = song.reshape((8,1,128,16))
+        song = data[stp:edp]
+        song = song.transpose(0, 1, 3, 2)
         X_tr.append(song)
         # print('i: {}, train_iex: {}, stp: {}, song.shape: {}, song num: {}'.format(i, train_idx[i], stp, song.shape, len(X_tr)))
 
@@ -65,20 +64,43 @@ def train_data(data,train_idx):
     return X_tr
 
 
+def train_chord(data, train_idx):
+    chord_tr = []
+    for i in range(train_idx.shape[0]):
+        stp = (train_idx[i])*8
+        edp = stp + 8
+        chord = data[stp:edp]
+        chord_tr.append(chord)
+    chord_tr = np.vstack(chord_tr)
+    return chord_tr
+
+def test_chord(data, test_idx):
+    chord_te = []
+    for i in range(test_idx.shape[0]):
+        stp = (test_idx[i])*8
+        edp = stp + 8
+        chord = data[stp:edp]
+        chord_te.append(chord)
+    chord_te = np.vstack(chord_te)
+    return chord_te
 
 # test_data
 X_te = test_data(data,test_idx)
 prev_X_te = test_data(prev_data,test_idx)
-np.save('octave2_X_te.npy',X_te)
-np.save('octave2_prev_X_te',prev_X_te)
+chord_te = test_chord(chord_data,test_idx)
+np.save('X_te.npy',X_te)
+np.save('prev_X_te',prev_X_te)
+np.save('chord_te.npy', chord_te)
 
 print('test song completed, X_te matrix shape: {}'.format(X_te.shape))
 
 #train_data
 X_tr = train_data(data,train_idx)
 prev_X_tr = train_data(prev_data,train_idx)
-np.save('octave2_X_tr.npy',X_tr)
-np.save('octave2_prev_X_tr.npy',prev_X_tr)
+chord_tr = train_chord(chord_data,train_idx)
+np.save('X_tr.npy',X_tr)
+np.save('prev_X_tr.npy',prev_X_tr)
+np.save('chord_tr.npy',chord_tr)
 
 print('train song completed, X_tr matrix shape: {}'.format(X_tr.shape))
 
